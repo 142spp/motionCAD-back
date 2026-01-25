f29b481ac99b469b409b9ea1492f4acd3cb38c8f
142spp <ehdrms3825@gmail.com>
2026-01-25 17:36:04 +0900

    feat: Initialize Spring Boot backend project with Gradle, JPA, and Docker support.

--- src/main/java/com/handcraft/puzzle/domain/DrawingProjectRepository.java
+++
@@ -1,6 +0,0 @@
-package com.handcraft.puzzle.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DrawingProjectRepository extends JpaRepository<DrawingProject, Long> {
-}
